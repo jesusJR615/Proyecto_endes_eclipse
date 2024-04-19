fb46553d4f0c9190571f6a2259594f86867abcc2
Jesus <jesusmartin@gmail.com>
2024-04-19 15:43:14 +0200

    En este proyecto no hay nada modificado

---
+++ src/Class/Principal.java
@@ -0,0 +1,28 @@
+package Class;
+
+import java.util.Scanner;
+public class Principal {
+	public static void main(String[]args) {
+		Scanner scanner = new Scanner (System.in);
+		/*Escribe un programa en Java que me diga si un número es divisible por 2, 3, 5 o 7
+(debe decir por todos los que es divisible).*/
+
+		System.out.println("Introduce cualquier numero: ");
+		double numero = scanner.nextDouble();
+		if (numero % 2 == 0) {
+			System.out.println("Es divisible por dos ");
+		}else {
+			System.out.println("No es divisible por dos ");
+		}if (numero % 3 == 0) {
+			System.out.println("Es divisible por tres ");
+		}else {
+			System.out.println("No es divisible por tres ");
+		}if (numero % 5 == 0 ){
+			System.out.println("Es divisible por cinco ");
+		} else if (numero % 7 == 0) {
+			System.out.println("Es divisible por siete ");
+		}else {
+			System.out.println("No es divisible ni por siete ni por cinco ");
+		}
+	}	
+}
